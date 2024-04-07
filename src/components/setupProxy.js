4e52f70dde0cdb5c5c9404392cdbe4c2c17e0ae9
NgynIT <nguyentun.1109@gmail.com>
2024-04-07 22:24:21 +0700

    fix error fecth news api successfully

--- src/components/setupProxy.js
+++ src/components/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
       target: 'https://newsapi.org',
       changeOrigin: true,
       pathRewrite: {
-        '^/api': '',
+        '^/api': 'https://newsapi.org/v2/everything?q=apple&from=2024-03-18&to=2024-03-18&sortBy=popularity&apiKey=242da07b4b7e4b7c8a7ea048be11cf3e',
       },
     })
   );
