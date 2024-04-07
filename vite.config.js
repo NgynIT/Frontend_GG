16c4bed6e2d523684d3a6472e79295ed459e6915
NgynIT <nguyentun.1109@gmail.com>
2024-04-07 23:13:43 +0700

    fix error fecth news api successfully

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    cors: {
+      origin: 'https://nguyennpcoder.netlify.app',  // Replace with your actual website URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Origin', 'Content-Type', 'Authorization'],
+    },
+  },
+});
