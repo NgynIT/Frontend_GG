0811fae05db845263fbb1307a5368da9d8e59932
NgynIT <nguyentun.1109@gmail.com>
2024-04-07 22:57:53 +0700

    fix error fecth news api successfully

--- src/components/setupProxy.js
+++ src/components/setupProxy.js
@@ -1,14 +1,27 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require('express');
+const fetch = require('node-fetch');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://newsapi.org',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': 'https://newsapi.org/v2/everything?q=apple&from=2024-03-18&to=2024-03-18&sortBy=popularity&apiKey=242da07b4b7e4b7c8a7ea048be11cf3e',
-      },
-    })
-  );
-};
+const app = express();
+const PORT = 5173;
+
+// Endpoint trung gian
+app.get('/api/news', async (req, res) => {
+  try {
+    const response = await fetch('https://newsapi.org/v2/everything?q=apple&from=2024-03-18&to=2024-03-18&sortBy=popularity&apiKey=242da07b4b7e4b7c8a7ea048be11cf3e');
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+    } else {
+      console.error('Failed to fetch news');
+      res.status(500).json({ error: 'Failed to fetch news' });
+    }
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    res.status(500).json({ error: 'Error fetching news' });
+  }
+});
+
+// Khởi chạy máy chủ
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on port ${PORT}`);
+});
